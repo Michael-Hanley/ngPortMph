9cf5e8337b422facd91c4424700f34a71235fb1e
Michael Hanley <pasaquali@gmail.com>
2017-06-17 17:58:47 -0700

    Material Design installed

--- ngPortMph/src/app/app.module.ts
+++ ngPortMph/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import 'hammerjs';
+import { MaterialModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'home', component: AppComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
